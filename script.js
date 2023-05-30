b9b91a37604940d85d14d7eb72bc622347ac84e7
royquillca <ccus.andy.quillca@gmail.com>
2023-05-30 15:49:12 -0500

    Personal information updated

--- script.js
+++ script.js
@@ -27,7 +27,8 @@
     $form.addEventListener("submit", (e) => {
         e.preventDefault();
         $loader.classList.remove("none");
-        fetch("https://formsubmit.co/ajax/royquillca@outlook.es", {
+        // fetch("https://formsubmit.co/ajax/royquillca@outlook.es", {
+        fetch("https://formsubmit.co/ajax/roquidata@gmail.com", {
             method:"POST",
             body:new FormData(e.target)
         }).then(res => (res.ok ? res.json(): Promise.reject(res)))
